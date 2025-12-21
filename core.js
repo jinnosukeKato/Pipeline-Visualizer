e8170fd7f01cfa8b546bca38ffdcb69716fe2fa2
KATO Jinnosuke <jinnosukekato@ms.dendai.ac.jp>
2025-12-21 07:04:35 +0000

    インスタンスに依存しないメソッドにstaticをつけた

--- core.js
+++ core.js
@@ -38,7 +38,7 @@ class Stage {
 }
 
 class HazardUnit {
-  detect(pipeline, forwardingEnabled) {
+  static detect(pipeline, forwardingEnabled) {
     const details = {
       hazardDetected: false,
       shouldStall: false,
@@ -50,7 +50,7 @@ class HazardUnit {
     return details;
   }
 
-  checkDataHazard(pipeline, forwardingEnabled, details) {
+  static checkDataHazard(pipeline, forwardingEnabled, details) {
     const idInstr = pipeline.ID.instruction;
     if (!idInstr) {
       return;
